aaab4043cf84e64f1a468bbbe5a0adc64aa6b65e
sebprt <sebastien.parrat@orange.fr>
2021-06-24 12:06:02 +0200

    Fixed AlternativeLookup and capacities, updated satellite-toolbox

--- src/Builder/Capacity/Lookup/All.php
+++ src/Builder/Capacity/Lookup/All.php
@@ -51,32 +51,37 @@ final class All implements Builder
             );
         }
 
-        return new Node\Expr\MethodCall(
-            var: new Node\Expr\MethodCall(
-                var: new Node\Expr\PropertyFetch(
-                    var: new Node\Expr\Variable('this'),
-                    name: new Node\Identifier('client')
-                ),
-                name: $this->endpoint
-            ),
-            name: new Node\Identifier('all'),
-            args: array_filter(
-                [
-                    new Node\Arg(
-                        value: new Node\Expr\Array_(
-                            items: $this->compileSearch(),
-                            attributes: [
-                                'kind' => Node\Expr\Array_::KIND_SHORT,
-                            ]
+        return new Node\Stmt\Expression(
+            expr: new Node\Expr\Assign(
+                var: new Node\Expr\Variable('lookup'),
+                expr:  new Node\Expr\MethodCall(
+                    var: new Node\Expr\MethodCall(
+                        var: new Node\Expr\PropertyFetch(
+                            var: new Node\Expr\Variable('this'),
+                            name: new Node\Identifier('client')
                         ),
-                        name: new Node\Identifier('queryParameters'),
+                        name: $this->endpoint
                     ),
-                    $this->code !== null ? new Node\Arg(
-                        value: $this->code,
-                        name: new Node\Identifier('attributeCode'),
-                    ) : null
-                ],
-            ),
+                    name: new Node\Identifier('all'),
+                    args: array_filter(
+                        [
+                            new Node\Arg(
+                                value: new Node\Expr\Array_(
+                                    items: $this->compileSearch(),
+                                    attributes: [
+                                        'kind' => Node\Expr\Array_::KIND_SHORT,
+                                    ]
+                                ),
+                                name: new Node\Identifier('queryParameters'),
+                            ),
+                            $this->code !== null ? new Node\Arg(
+                                value: $this->code,
+                                name: new Node\Identifier('attributeCode'),
+                            ) : null
+                        ],
+                    ),
+                )
+            )
         );
     }
 
--- src/Builder/Capacity/Lookup/Download.php
+++ src/Builder/Capacity/Lookup/Download.php
@@ -3,6 +3,7 @@
 namespace Kiboko\Plugin\Akeneo\Builder\Capacity\Lookup;
 
 use Kiboko\Component\SatelliteToolbox\Builder\IsolatedCodeBuilder;
+use Kiboko\Component\SatelliteToolbox\Builder\IsolatedValueAppendingBuilder;
 use Kiboko\Plugin\Akeneo\MissingEndpointException;
 use PhpParser\Builder;
 use PhpParser\Node;
@@ -44,116 +45,121 @@ final class Download implements Builder
             );
         }
 
-        return (new IsolatedCodeBuilder([
-            new Node\Stmt\If_(
-                cond: new Node\Expr\FuncCall(
-                    name: new Node\Name('is_null'),
-                    args: array_filter(
-                        [
-                            $this->code !== null ? new Node\Arg(
-                                value: $this->code,
-                            ) : null
-                        ],
+        return (new IsolatedValueAppendingBuilder(
+            new Node\Expr\Variable('input'),
+            new Node\Expr\Variable('lookup'),
+            [
+                new Node\Stmt\If_(
+                    cond: new Node\Expr\FuncCall(
+                        name: new Node\Name('is_null'),
+                        args: array_filter(
+                            [
+                                $this->code !== null ? new Node\Arg(
+                                    value: $this->code,
+                                ) : null
+                            ],
+                        ),
                     ),
+                    subNodes: [
+                        'stmts' => [
+                            new Node\Stmt\Return_(
+                                expr: new Node\Expr\ConstFetch(
+                                    name: new Node\Name(name: 'null'),
+                                ),
+                            )
+                        ],
+                    ],
                 ),
-                subNodes: [
-                    'stmts' => [
-                        new Node\Stmt\Return_(
-                            expr: new Node\Expr\ConstFetch(
-                                name: new Node\Name(name: 'null'),
+                new Node\Stmt\TryCatch(
+                    stmts: [
+                        new Node\Stmt\Expression(
+                            expr: new Node\Expr\Assign(
+                                var: new Node\Expr\Variable('image'),
+                                expr: new Node\Expr\MethodCall(
+                                    var: new Node\Expr\MethodCall(
+                                        var: new Node\Expr\MethodCall(
+                                            var: new Node\Expr\MethodCall(
+                                                var: new Node\Expr\PropertyFetch(
+                                                    var: new Node\Expr\Variable('this'),
+                                                    name: new Node\Identifier('client'),
+                                                ),
+                                                name: $this->endpoint
+                                            ),
+                                            name: new Node\Identifier('download'),
+                                            args: array_filter(
+                                                [
+                                                    $this->code !== null ? new Node\Arg(
+                                                        value: $this->code,
+                                                        name: new Node\Identifier('code'),
+                                                    ) : null
+                                                ],
+                                            ),
+                                        ),
+                                        name: new Node\Identifier('getBody'),
+                                    ),
+                                    name: new Node\Identifier('getContents'),
+                                ),
                             ),
                         )
                     ],
-                ],
-            ),
-            new Node\Stmt\TryCatch(
-                stmts: [
-                    new Node\Stmt\Expression(
-                        expr: new Node\Expr\Assign(
-                            var: new Node\Expr\Variable('image'),
-                            expr: new Node\Expr\MethodCall(
-                                var: new Node\Expr\MethodCall(
-                                    var: new Node\Expr\MethodCall(
-                                        var: new Node\Expr\MethodCall(
-                                            var: new Node\Expr\PropertyFetch(
-                                                var: new Node\Expr\Variable('this'),
-                                                name: new Node\Identifier('client'),
+                    catches: [
+                        new Node\Stmt\Catch_(
+                            types: [
+                                new Node\Name\FullyQualified('Akeneo\Pim\ApiClient\Exception\HttpException'),
+                            ],
+                            var: new Node\Expr\Variable('exception'),
+                            stmts: [
+                                new Node\Stmt\Expression(
+                                    expr: new Node\Expr\MethodCall(
+                                        var: new Node\Expr\PropertyFetch(
+                                            var: new Node\Expr\Variable('this'),
+                                            name: 'logger',
+                                        ),
+                                        name: new Node\Identifier('error'),
+                                        args: [
+                                            new Node\Arg(
+                                                value: new Node\Expr\MethodCall(
+                                                    var: new Node\Expr\Variable('exception'),
+                                                    name: new Node\Identifier('getMessage'),
+                                                ),
                                             ),
-                                            name: $this->endpoint
-                                        ),
-                                        name: new Node\Identifier('download'),
-                                        args: array_filter(
-                                            [
-                                                $this->code !== null ? new Node\Arg(
-                                                    value: $this->code,
-                                                    name: new Node\Identifier('code'),
-                                                ) : null
-                                            ],
-                                        ),
+                                            new Node\Arg(
+                                                value: new Node\Expr\Array_(
+                                                    items: [
+                                                        new Node\Expr\ArrayItem(
+                                                            value: new Node\Expr\Variable('exception'),
+                                                            key: new Node\Scalar\String_('exception'),
+                                                        ),
+                                                    ],
+                                                    attributes: [
+                                                        'kind' => Node\Expr\Array_::KIND_SHORT,
+                                                    ],
+                                                ),
+                                            ),
+                                        ],
                                     ),
-                                    name: new Node\Identifier('getBody'),
                                 ),
-                                name: new Node\Identifier('getContents'),
-                            ),
+                                new Node\Stmt\Expression(
+                                    expr: new Node\Expr\MethodCall(
+                                        var: new Node\Expr\Variable('bucket'),
+                                        name: new Node\Identifier('reject'),
+                                        args: [
+                                            new Node\Arg(
+                                                new Node\Expr\Variable('input'),
+                                            )
+                                        ],
+                                   )
+                                )
+                            ],
                         ),
-                    )
-                ],
-                catches: [
-                    new Node\Stmt\Catch_(
-                        types: [
-                            new Node\Name\FullyQualified('Akeneo\Pim\ApiClient\Exception\HttpException'),
-                        ],
-                        var: new Node\Expr\Variable('exception'),
-                        stmts: [
-                            new Node\Stmt\Expression(
-                                expr: new Node\Expr\MethodCall(
-                                    var: new Node\Expr\PropertyFetch(
-                                        var: new Node\Expr\Variable('this'),
-                                        name: 'logger',
-                                    ),
-                                    name: new Node\Identifier('error'),
-                                    args: [
-                                        new Node\Arg(
-                                            value: new Node\Expr\MethodCall(
-                                                var: new Node\Expr\Variable('exception'),
-                                                name: new Node\Identifier('getMessage'),
-                                            ),
-                                        ),
-                                        new Node\Arg(
-                                            value: new Node\Expr\Array_(
-                                                items: [
-                                                    new Node\Expr\ArrayItem(
-                                                        value: new Node\Expr\Variable('exception'),
-                                                        key: new Node\Scalar\String_('exception'),
-                                                    ),
-                                                ],
-                                                attributes: [
-                                                    'kind' => Node\Expr\Array_::KIND_SHORT,
-                                                ],
-                                            ),
-                                        ),
-                                    ],
-                                ),
-                            ),
-                            new Node\Stmt\Expression(
-                                expr: new Node\Expr\MethodCall(
-                                    var: new Node\Expr\Variable('bucket'),
-                                    name: new Node\Identifier('reject'),
-                                    args: [
-                                        new Node\Expr\Variable('input'),
-                                    ],
-                               )
-                            )
-                        ],
-                    ),
-                ],
-            ),
-            new Node\Stmt\Return_(
-                expr: new Node\Expr\Variable('image'),
-            ),
-        ],
-        new Node\Expr\Variable('bucket'),
-        new Node\Expr\Variable('input'),))->getNode();
+                    ],
+                ),
+                new Node\Stmt\Return_(
+                    expr: new Node\Expr\Variable('image'),
+                ),
+            ],
+            new Node\Expr\Variable('bucket')
+        ))->getNode();
     }
 
     private function compileSearch(): array
