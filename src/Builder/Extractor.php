f8e35dafb904b5eaa11fbc417edea63e242982f6
sebprt <sebastien@kiboko.fr>
2023-07-27 09:57:05 +0200

    Ran php-cs-fixer

--- src/Builder/Extractor.php
+++ src/Builder/Extractor.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Kiboko\Plugin\Akeneo\Builder;
 
-use Akeneo\Pim\ApiClient\Exception\UnprocessableEntityHttpException;
 use Kiboko\Contract\Configurator\StepBuilderInterface;
 use PhpParser\Builder;
 use PhpParser\Node;
