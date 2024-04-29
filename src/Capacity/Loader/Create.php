235e8d742bb9f90ac56c58be3f86da5b7b7b25c2
Jonathan <jonathan@kiboko.fr>
2024-04-29 12:33:14 +0200

    use data product from line instead of full line item

--- src/Capacity/Loader/Create.php
+++ src/Capacity/Loader/Create.php
@@ -31,17 +31,8 @@ final class Create implements Akeneo\Capacity\CapacityInterface
         $builder = (new Akeneo\Builder\Capacity\Loader\Create())
             ->withEndpoint(endpoint: new Node\Identifier(sprintf('get%sApi', ucfirst((string) $config['type']))))
             ->withCode(code: compileValueWhenExpression($this->interpreter, $config['code'], 'line'))
-            ->withData(line: new Node\Expr\Variable('line'))
+            ->withData(line: new Node\Expr\Variable('line["product"]'))
         ;
-
-        if (\array_key_exists('reference_entity', $config)) {
-            $builder->withReferenceEntity(referenceEntity: new Node\Scalar\String_($config['reference_entity']));
-        }
-
-        if (\array_key_exists('reference_entity_attribute', $config)) {
-            $builder->withReferenceEntityAttribute(referenceEntityAttribute: compileValueWhenExpression($this->interpreter, $config['reference_entity_attribute'], 'line'));
-        }
-
         return $builder;
     }
 }
